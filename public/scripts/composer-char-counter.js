07f70d0ecc365836fb070f88e797f809ede5298f
Eric Dokter <edokter11@gmail.com>
2022-03-21 14:21:33 -0600

    added char count

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function () {
+    const maxChar = 140
+    let tweetLength = $(this).val().length;
+    let remaining = maxChar - tweetLength;
+    $(".counter").text(remaining)
+
+    if (remaining < 0) {
+      $(".counter").css("color", "red")
+    }
+    console.log(remaining)
+  })
+});
+
