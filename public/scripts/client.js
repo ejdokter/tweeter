42f37f6820e251b59f463c2b8c75b475e7311941
Eric Dokter <edokter11@gmail.com>
2022-04-01 13:46:14 -0600

    final touches and screenshots added

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,48 +4,27 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png"
-//       ,
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": "https://i.imgur.com/nlhLi3I.png",
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   }
-// ]
 
 $(document).ready(function() {
 
+  // renderTweets takes in an array of tweet objects and appends each one to the #tweet-container
   const renderTweets = function(tweets) {
     for (const tweet of tweets) {
-      const $tweet = createTweetElement(tweet)
-      $('section.tweet-container').prepend($tweet)
+      const $tweet = createTweetElement(tweet);
+      $('section.tweet-container').prepend($tweet);
     }
-  }
+  };
 
-  const escape = function (str) {
+  // escape function to prevent xss attacks
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
-  }
+  };
 
+  // creating tweets using html template
   const createTweetElement = function(tweetData) {
-    const $tweet = $(`<article class="tweet"></article>`)
+    const $tweet = $(`<article class="tweet"></article>`);
 
     const html = `
     <header>
@@ -62,52 +41,92 @@ $(document).ready(function() {
               <i class='fa fa-heart'></i>
             </div>
           </footer>
-    `
-    $tweet.append(html)
-    return $tweet
-  }
+    `;
+    $tweet.append(html);
+    return $tweet;
+  };
 
+  //ajax request to load tweets from /tweets
+  const loadTweets = function() {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function(tweets) {
+        renderTweets(tweets);
+      });
+  };
+  
+  loadTweets();
 
-  const $newTweet = $('#new-tweet');
+  //error message when incorrectly composing a tweet
+  const $newTweet = $('#tweet-form');
 
   $newTweet.on('submit', function(event) {
-    $("#error-message").slideUp()
-    event.preventDefault();
-    console.log('new tweet created');
+    $("#error-message").slideUp(); //slide up any current message
+    event.preventDefault(); //prevent refresh
 
-    if(!$newTweet.children('textarea').val()) {
-      $("#error-message span").text('Tweet cannot be blank')
-      $("#error-message").slideDown()
-    } else if ($newTweet.children('textarea').val().length > 140) {
-      $("#error-message span").text('Tweet cannot exceed 140 characters')
-      $("#error-message").slideDown()
-    } else {
+    if (!$newTweet.children('textarea').val()) { //if tweet is blank
+      $("#error-message span").text('Tweet cannot be blank');
+      $("#error-message").slideDown();
+      return;
+    }
 
-        const serializedData = $(event.target).serialize()
+    if ($newTweet.children('textarea').val().length > 140) { //if tweet exceeds 140 characters
+      $("#error-message span").text('Tweet cannot exceed 140 characters');
+      $("#error-message").slideDown();
+      return;
+    }
 
-        $.post('/tweets', serializedData, response => {
-          console.log(response)
-          loadTweets()
-        })
-      }
-  })
+    //post request to create tweet from data
+    const serializedData = $(event.target).serialize();
+    $.ajax({url: '/tweets', method: 'POST', data: serializedData})
+      .then(function() {
+        return $.get('/tweets');
+      })
+      .then(function(response) {
+        const latestTweet = [response[response.length - 1]];
+        renderTweets(latestTweet);
+      });
+    $newTweet.trigger('reset');
+  });
 
-  const loadTweets = function () {
-    $.ajax('/tweets', { method: 'GET' })
-    .then(function(tweets) {
-      renderTweets(tweets)
-    })
-  }
-
-  loadTweets()
-
-  const $rightHeader = $('#rightHeader')
+  // toggling the tweet container and focusing it
+  const $rightHeader = $('#rightHeader');
 
   $rightHeader.on('click', function(event) {
-    $('#new-tweet').slideToggle()
-    $('#tweet-text').focus()
-    event.preventDefault()
-    console.log('input test')
-  })
+    $('#new-tweet').slideToggle();
+    $('#tweet-text').focus();
+    event.preventDefault();
+  });
 
-})
+  // scrolling to the top when clicking the button
+  const $scrollTop = $('#scroll-top');
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > 200) {
+      $scrollTop.addClass('show');
+    } else {
+      $scrollTop.removeClass('show');
+    }
+  });
+
+  $scrollTop.on('click', function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop:0}, '300');
+    $('#new-tweet').slideDown();
+    $('#tweet-text').focus();
+  });
+
+  //hiding the nav when scrolling down
+  const $nav = $('nav');
+  let prevScroll = window.pageYOffset;
+
+  $(window).scroll(function() {
+    let currentScroll = window.pageYOffset;
+    if (prevScroll > currentScroll) {
+      $nav.css('top', '0');
+    } else {
+      $nav.css('top','-120px');
+    }
+    prevScroll = currentScroll;
+  });
+
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,15 @@
+//character counter for text area
+
 $(document).ready(function() {
-  $("#tweet-text").on("input", function () {
-    const maxChar = 140
+  $("#tweet-text").on("input", function() {
+    const maxChar = 140;
     let tweetLength = $(this).val().length;
     let remaining = maxChar - tweetLength;
-    $(".counter").text(remaining)
+    $(".counter").text(remaining);
 
     if (remaining < 0) {
-      $(".counter").css("color", "red")
+      $(".counter").css("color", "red");
     }
-  })
+  });
 });
 
